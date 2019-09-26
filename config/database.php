a106b0d5956e5ac0572b443e21612e89e979b7ac
Kaikawa1028 <a.kousuke0828@gmail.com>
2019-09-26 16:59:49 +0900

    config修正

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'test',
+            'database' => 'circle_test',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
